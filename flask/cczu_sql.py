05f9069445ef5095b099e97b0c3e827e2521daf9
Kejian Zhao <gatersbolton@qq.com>
2023-06-21 19:39:38 +0800

    用sqlite操作数据库的接口

--- flask/cczu_sql.py
+++ flask/cczu_sql.py
@@ -1,48 +1,123 @@
+import os
 import sqlite3
 
-#用sqlite3进行一些数据库操作
 class sql:
-    #shuaige
-    sqlfile='cczu.db'#数据库的名字
+    sqlfile = 'cczu.db'  # 数据库的名字
+    def __init__(self):
+        pass
     # 参数全是字符串
     # 几个数据表：
     # users(wechat_id,username,password)
     # preference(wechat_id,floor,seat,starttime,duration)
-    def init(self):
-        #不是__init__这个生成函数
-        # 初始化，建数据库，建表之类的，总共只要用一次
-        pass
-    def auto_login(self,wechat_id):
-        pass
-        #查users表是否有wechat_id=wechat_id的条目
-        if True:
-            username='20410000';password='123456';#这里设置成表里查到的
-            return username,password
+    def initsql(self):
+        # 不是__init__这个生成函数
+        # 初始化，建数据库，建表y之类的，总共只要用一次
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+        # 连接到数据库，如果不存在则会自动创建
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+        # 创建用户表
+        c.execute('''CREATE TABLE IF NOT EXISTS users (
+                        wechat_id TEXT PRIMARY KEY NOT NULL,
+                        username TEXT NOT NULL,
+                        password TEXT NOT NULL,
+                        seat TEXT,
+                        floor TEXT,
+                        starttime TEXT,
+                        duration TEXT
+                    )''')
+        conn.commit()
+        conn.close()
+    def auto_login(self, wechat_id):
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+        # 查询 users 表
+        c.execute("SELECT username,password FROM users WHERE wechat_id=?", (wechat_id,))
+        result = c.fetchone()
+        conn.close()
+        # 处理结果
+        if result is not None:
+            username, password = result
         else:
-            username = 'none';password = 'none';
-            return username,password
+            username = 'none';
+            password = 'none';
+        return username, password
 
-    def bind(self,wechat_id, username,password):
-        pass
-        #添加users(wechat_id, username,password)到表里，并添加preference(wechat_id,floor,seat,starttime,duration)，初值全是字符串的'none'
+    def bind(self, wechat_id, username, password):
+        """添加 (wechat_id, username, password) 到 users 表，并添加对应的偏好表项，初值全是字符串的 'none'"""
+        # 连接到数据库
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+        # 添加用户信息到 users 表
+        c.execute("INSERT INTO users (wechat_id, username, password, seat, floor, starttime, duration) VALUES (?, ?, ?, ?, ?, ? ,?)", (wechat_id, username, password, 'none', 'none', 'none', 'none'))
+        conn.commit()
+        conn.close()
 
-    def getpreference(self,wechat_id):
-        pass
-        #查wechat_id对应的preference表，返回
-        #return wechat_id,floor,seat,starttime,duration
+    def getpreference(self, wechat_id):
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
 
-    def setpreference(self,wechat_id,floor,seat,starttime,duration):
-        #参数可缺省，默认为字符串的"none"
-        pass
-        #修改preference表里的对应项
+        # 查询 preference 表
+        c.execute("SELECT floor, starttime, duration FROM users WHERE wechat_id=?", (wechat_id,))
+        result = c.fetchone()
+        conn.close()
+        # 处理结果
+        if result is not None:
+            floor, starttime, duration = result
+        else:
+            floor = 'none';
+            starttime = 8.0;
+            duration = 4.0;
 
-if __name__=='__main__':
-    # 示例：
-    cczusql=sql
-    cczusql.init()
-    username,password=cczusql.auto_login(wechat_id='1234567890abcdefg')
-    if(username=='none'):
-        username='12345678'
-        password='123456'
-        cczusql.bind(wechat_id='1234567890abcdefg',username=username,password=password)
-    # preference也类似
+        return wechat_id, floor, starttime, duration
+
+    # 查wechat_id对应的preference表，返回
+        # return wechat_id,floor,seat,starttime,duration
+
+    def setpreference(self, wechat_id, floor='none', seat='none', starttime='none', duration='none'):
+        # 参数可缺省，默认为字符串的"none"
+        # 连接到数据库
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+
+        # 修改 preference 表
+        c.execute("UPDATE users SET floor=?, starttime=?, duration=? WHERE wechat_id=?",
+                  (floor, starttime, duration, wechat_id))
+        conn.commit()
+        conn.close()
+        # 修改preference表里的对应项
+    def select_all(self):
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+        # 查询 users 表
+        c.execute("SELECT * FROM users")
+        result = c.fetchall()
+        conn.close()
+        return result
+
+    def del_by_wechatid(self,wechat_id):
+        # 建立数据库连接并获取游标对象
+        conn = sqlite3.connect(self.sqlfile)
+        c = conn.cursor()
+        c.execute("DELETE FROM users WHERE wechat_id=?", (wechat_id,))
+        # 提交更改并关闭连接
+        conn.commit()
+        conn.close()
+
+
+if __name__ == '__main__':
+    cczusql = sql()
+    cczusql.initsql()
+    wechat_id='1234567890abcdefgg'
+    username, password = cczusql.auto_login(wechat_id=wechat_id)
+
+    cczusql.del_by_wechatid(wechat_id=wechat_id)
+    # if (username == 'none'):
+    #     username = '12345678'
+    #     password = '123456'
+    #     cczusql.bind(wechat_id=wechat_id, username=username, password=password)
+
+    # os.remove(cczusql.sqlfile)
+    print(username, password)
+    print(cczusql.select_all())
